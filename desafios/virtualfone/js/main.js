ee30a56cbc6e97cd7d573443c9463143df7e229e
Cauan-G <cauanguimaraespro@gmail.com>
2023-06-05 22:24:52 -0300

    lab virtual fone 05/06

--- desafios/virtualfone/js/main.js
+++ desafios/virtualfone/js/main.js
@@ -1,9 +1,25 @@
 
 //Pega os valores de todos os botões
 const listaTeclas = document.querySelectorAll('input[type=button]');
+const display =  document.querySelector('input[type=tel]');
 
-function valorBotaoUm (){ // mostra o valor do primeiro botão
-    alert(listaTeclas[0].value);
+// function valorBotaoUmJanela (){ // mostra o valor do primeiro botão
+//     valorBotaoUm(listaTeclas[0].value);
+// }
+// function valorBotaoUm(){
+
+//     display.value = display.value + listaTeclas[0].value;
+// }
+// listaTeclas[0].onclick = valorBotaoUm; // chama a função quando clicado
+
+function valorBotao(valor){
+    display.value = display.value + listaTeclas[valor] .value
 }
 
-listaTeclas[0].onclick = valorBotaoUm; // chama a função quando clicado
+function limpaDisplay(){
+    display.value = '' // coloca o valor do display como nulo
+}
+
+listaTeclas[0].onclick = function(){
+    valorBotao(11);     
+}
